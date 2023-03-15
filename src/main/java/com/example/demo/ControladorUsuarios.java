3f364dcd67488551c486ab90f67b490da1f1a7ad
prbm13 <123487449+prbm13@users.noreply.github.com>
2023-03-15 11:30:49 +0100

    clase ControladorUsuarios agregada

---
+++ src/main/java/com/example/demo/ControladorUsuarios.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //  Y ADEMAS, PREPARATE PARA RECIBIR PETICIONES HTTP (REST)
+public class ControladorUsuarios {
+
+    @RequestMapping("/")
+    String hellow()
+    {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/hola")
+    String holam()
+    {
+        return " Hola Mundo!";
+    }
+
+    @RequestMapping("/mimedicacion")
+    String mimedicacion()
+    {
+        return "Tomate las pastillas!";
+    }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,33 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController //  Y ADEMAS, PREPARATE PARA RECIBIR PETICIONES HTTP (REST)
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@RequestMapping("/")
-	String hellow()
-	{
-		return "Hello World!";
-	}
-
-	@RequestMapping("/hola")
-	String holam()
-	{
-		return " Hola Mundo!";
-	}
-
-	@RequestMapping("/mimedicacion")
-	String mimedicacion()
-	{
-		return "Tomate las pastillas!";
-	}
-
 }
